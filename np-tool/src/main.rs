d723d1b528939cf99a009dd63e3ebff1d2b1c0ac
borovan <84897664+borovan@users.noreply.github.com>
2025-03-25 12:46:09 +0100

    added more exchanges

--- np-tool/src/main.rs
+++ np-tool/src/main.rs
@@ -64,20 +64,25 @@ fn main() {
 }
 
 const EXCHANGES: &[(&str, &str)] = &[
-    ("Binance 1", "220c3a33f90601896e26f76fa619fe288742df1fa75426edfaf759d39f2455a5"),
-    ("Binance 2", "d3e13d4777e22367532053190b6c6ccf57444a61337e996242b1abfb52cf92c8"),
+    ("Bitget", "bad030b417484232fd2019cb89096feea3fdd3d9eb39e1d07bcb9a13c7673464"),
+    ("Binance 1", "609d3e1e45103a82adc97d4f88c51f78dedb25701e8e51e8c4fec53448aadc29"),
+    ("Binance 2", "220c3a33f90601896e26f76fa619fe288742df1fa75426edfaf759d39f2455a5"),
+    ("Binance 3", "d3e13d4777e22367532053190b6c6ccf57444a61337e996242b1abfb52cf92c8"),
+    ("Bybit", "acd76fff0536f863d9dd4b326a1435466f82305758b4b1b4f62ff9fa81c14073"),
     ("Coinbase 1", "449ce7ad1298e2ed2781ed379aba25efc2748d14c60ede190ad7621724b9e8b2"),
     ("Coinbase 2", "4dfa940def17f1427ae47378c440f10185867677109a02bc8374fc25b9dee8af"),
     ("Coinbase 3", "a6ed987d89796f921c8a49d275ec7c9aa04e75a8fc8cd2dbaa5da799f0215ab0"),
     ("Coinbase 4", "660b1680dafeedaa68c1f1f4cf8af42ed1dfb8564646efe935a2b9a48528b605"),
     ("Coinbase 5", "dd15f3040edab88d2e277f9d2fa5cc11616ebf1442279092e37924ab7cce8a74"),
     ("Coinbase 6", "4878d23a09b554157b31323004e1cc053567671426ca4eec7b7e835db607b965"),
-    ("HTX 1", "935b1a3adc28fd68cacc95afcdec62e985244ce0cfbbb12cdc7d0b8d198b416d"),
+    ("Gate.io", "8fe706db7b08f957a15199e07761039a7718937aabcc0fe48bc380a4daf9afb0"),
+    ("HTX", "935b1a3adc28fd68cacc95afcdec62e985244ce0cfbbb12cdc7d0b8d198b416d"),
     ("Kraken", "040834c30cdf5d7a13aae8b57d94ae2d07eefe2bc3edd8cf88298730857ac2eb"),
     ("KuCoin 1", "efa01544f509c56dd85449edf2381244a48fad1ede5183836229c00ab00d52df"),
     ("KuCoin 2", "00c3df112e62ad353b7cc7bf8ad8ce2fec8f5e633f1733834bf71e40b250c685"),
     ("MEXC", "9e62737aab36f0baffc1faac9edd92a99279723eb3feb2e916fa99bb7fe54b59"),
     ("OKX 1", "e7a879ea563d273c46dd28c1584eaa132fad6f3e316615b3eb657d067f3519b5"),
+    ("OKX 2", "d2c6135510eaf107bdc2128ef5962c7db2ae840efdf95b9395cdaf4983942978"),
 ];
 
 const FOUNDATION: &[&str] = &[
